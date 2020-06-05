211ac24e24dc4b75b675ed6601eae399662a3e77
Ayesha Jownally <sheik.bibi.ayesha.siddiqa.jownally@elca.mu>
2020-06-05 17:05:49 +0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  import Product from "./Product.svelte";
+
+  let products = [
+    {
+      id: "p1",
+      title: "A Book",
+      price: 9.99
+    }
+  ];
+
+  //Extract event data
+  function addToCart(event) {
+    console.log(event);
+  }
+
+  function deleteProduct(event) {
+    console.log(event);
+  }
+</script>
+
+<style>
+
+</style>
+
+<main>
+  {#each products as product}
+    <!--Emmit Custom Events-->
+    <Product
+      {...product}
+      on:add-to-cart={addToCart}
+      on:delete={deleteProduct} />
+  {/each}
+
+</main>
---
+++ src/Product.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  export let title;
+  export let price;
+  export let bestseller = false;
+  //Emit Custom Events
+  const dispatch = createEventDispatcher();
+
+  function addTocart() {
+    dispatch("add-to-cart", { id: "p1" });
+  }
+  //////////////////////////////////////////////////
+</script>
+
+<article>
+  <h1>{title}</h1>
+  <h2>Rs {price}</h2>
+  {#if bestseller}
+    <h3>bestseller</h3>
+  {/if}
+  <!--Event Forwarding - add function on App-->
+  <button on:click={addTocart}>Add to cart</button>
+  <button
+    on:click={() => {
+      dispatch('delete', 'p1');
+    }}>
+    Delete
+  </button>
+</article>
